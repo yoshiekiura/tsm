0cc45f199ddc6ac0a7ab32914d21b386b0739d9d
Hanan Kusuma <developer08@esoftdream.co.id>
2017-03-02 15:22:34 +0700

    check crossline

---
+++ _application/modules/rekap/controllers/check_crosline.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ * Description of check_crosline
+ *
+ * @author mang_haku
+ * @copyright (c) year, ESD
+ */
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class check_crosline extends MY_Controller{
+    function __construct() {
+        parent::__construct();
+        
+    }
+    
+    function index() {
+        $sql = 'SELECT network_id, network_sponsor_network_id, network_upline_network_id
+            FROM sys_network
+            ';
+        
+        $arr_member = $this->db->query($sql);
+        
+        if($arr_member->num_rows() >0){
+            foreach ($arr_member->result() as $row) {
+                $is_crossline = $this->mlm_function->check_uplink($row->network_upline_network_id, $row->network_sponsor_network_id);
+                
+                if(!$is_crossline) {
+                    echo $network_id . ' Crossline <br>';
+                }
+            }
+        }
+    }
+}
